77a8d187f7521f05db8979af0cd2018505c47553
CaMarchPa <chouenyib.19@gmail.com>
2017-09-27 15:50:20 +0200

    correction des erreurs

--- KDNode.py
+++
@@ -1,6 +0,0 @@
-class KDNode:
-    def __init__(self, pivot = None, axis = None, left_child = None, right_child = None):
-        self.pivot       = pivot
-        self.axis        = axis
-        self.left_child  = left_child
-        self.right_child = right_child
---
+++ src/KDNode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class KDNode:
+	def __init__(self, pivot = None, axis = None, left_child = None, right_child = None):
+		self.pivot = pivot
+		self.axis = axis
+		self.left_child = left_child
+		self.right_child = right_child
+def test(self):
+	return 0
